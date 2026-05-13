05609e4e81a828ecf81dc28815137955f636c837
TrisHorsh <tranthientrieu2004@gmail.com>
2026-05-13 16:22:51 +0700

    fix

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-app.use(express.json());
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
